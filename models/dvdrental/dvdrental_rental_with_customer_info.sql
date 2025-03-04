2dd3d57b84a17e02ca5de8c78a2e8dff69ff2a51
Lai <peichunl@UMICH.EDU>
2025-03-04 14:35:00 -0500

    03042025 seconc commit

---
+++ models/dvdrental/dvdrental_rental_with_customer_info.sql
@@ -0,0 +1,34 @@
+with source_rental as (
+    select * from {{source('dvdrental_source','rental')}}
+),
+
+source_customer as (
+    select * from {{source('dvdrental_source','customer')}}
+),
+
+active_rental as (
+    select r.customer_id,
+    count(*) as active_rentals_count
+    from source_rental r
+    where r.return_date is null
+    group by 
+        r.customer_id
+),
+
+final as (
+    select
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        coalesce(ar.active_rentals_count, 0) as active_rentals_count
+    from
+    source_customer c
+    left join
+        active_rental ar on c.customer_id = ar.customer_id
+    where
+        c.last_update > current_date - interval '1 year'
+    order by
+    c.customer_id    
+)
+
+select * from final
